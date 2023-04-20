389ef625687b83737ad84eddcda1a78699bd4263
9898d <shiganov.da@edu.spbstu.ru>
2023-04-20 23:02:08 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -6,4 +6,19 @@ function getRandomInt(min, max) {
 
 function isMaxLength(str, maxLength) {
   return (str.length <= maxLength);
-};
+}
+
+let photos = [];
+
+function createArrayOfUsers(numberOfUsers) {
+  for (let i = 1; i <= numberOfUsers; i++) {
+  let photo = {
+    id: i,
+    url: `photos/${i}.jpg`,
+    description: `Фото номер ${i}`,
+    likes: getRandomInt(15, 200),
+    comments: getRandomInt(0, 200)
+  }
+  photos.push(photo);
+}}
+createArrayOfUsers(25);
